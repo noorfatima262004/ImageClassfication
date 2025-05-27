a530ebd80afb429ede4b55d096d1e172a8f5f2b4
Noor Fatima <noorfatima262004@gmail.com>
2025-05-27 13:34:31 +0500

    pangy bazi

--- client/src/components/Location.jsx
+++ client/src/components/Location.jsx
@@ -28,7 +28,7 @@ const Location = () => {
     { 
       name: "Abeer dev branch",
       name: "Abeer main branch new again",
-      // name: "Abeer dev branch",
+      name: "Abeer dev branch comment out it",
       name: "Abeer main branch remove it bz its dev branch",
       role: "Lead Developer",
       bio: "Full-stack developer and machine learning engineer",
